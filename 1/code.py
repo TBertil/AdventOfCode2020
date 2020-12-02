4f228982b486f9a60e6c4728cb4b8a7c998f7816
Tobias bertilsson <Bertilsson.tobias@gmail.com>
2020-12-02 08:55:42 +0100

    First day added sec

--- 1/code.py
+++ 1/code.py
@@ -4,19 +4,31 @@ with open("C:\\Users\\berti\\Documents\\git\\AdventOfCode2020\\1\\input.txt","r"
     for line in f:
         numbers.append(int(line))
 
-savedNr = []
-multiplied = 0
+def getTwoNrs():
+    while(len(numbers)):
+        baseNr = numbers.pop()
+        for secNr in numbers:
+            if(secNr != baseNr):
+                if(2020 == (baseNr+secNr)):
+                    print("Two numbers:")
+                    print(baseNr)
+                    print(secNr)
+                    print(baseNr * secNr)
+                    return
 
-def getNrs():
+
+def getThreeNrs():
     while(len(numbers)):
         baseNr = numbers.pop()
         for secNr in numbers:
             for thirdNr in numbers:
                 if(secNr != thirdNr):
                     if(2020 == (baseNr+secNr+thirdNr)):
+                        print("Three numbers:")
                         print(baseNr)
                         print(secNr)
                         print(thirdNr)
                         print(baseNr * secNr * thirdNr)
                         return
-getNrs()
+getTwoNrs()
+getThreeNrs()
